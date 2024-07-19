580d9252c6c7421a18068ffecc5e1c855f4d1ab6
Henadzi Shutko <hazardsoft@gmail.com>
2024-07-19 07:27:42 +0300

    feat: use esbuild to compile project

---
+++ esbuild.config.mjs
@@ -0,0 +1,24 @@
+import * as esbuild from 'esbuild';
+
+const options = {
+  format: 'cjs',
+  bundle: true,
+  platform: 'node',
+
+  sourcemap: false,
+  minify: true,
+  tsconfig: 'tsconfig.build.json',
+  external: [
+    'class-validator',
+    'class-transformer',
+    '@nestjs/microservices',
+    '@nestjs/microservices/microservices-module',
+    '@nestjs/websockets/socket-module',
+  ],
+};
+
+await esbuild.build({
+  entryPoints: ['src/main.ts'],
+  outfile: 'dist/handlers/cart/cart.js',
+  ...options,
+});
