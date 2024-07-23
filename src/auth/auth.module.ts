6def9362a5a1bb67eb1e605555065bd2ce422429
Henadzi Shutko <hazardsoft@gmail.com>
2024-07-23 22:00:01 +0300

    refactor: remove PrismaModule import in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,18 +3,11 @@ import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { BasicStrategy } from './strategies';
-
 import { UsersModule } from '../users/users.module';
-import { PrismaModule } from '../db/prisma.module';
-import { BasicAuthGuard } from './guards';
 
 @Module({
-  imports: [
-    UsersModule,
-    PassportModule.register({ defaultStrategy: 'basic' }),
-    PrismaModule,
-  ],
-  providers: [AuthService, BasicStrategy, BasicAuthGuard],
+  imports: [UsersModule, PassportModule.register({ defaultStrategy: 'basic' })],
+  providers: [AuthService, BasicStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
