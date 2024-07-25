03c7705b8570e46f8502330afb151fe159432f2e
Henadzi Shutko <hazardsoft@gmail.com>
2024-07-25 10:31:09 +0300

    refactor: move aws cdk account id/region env vars to config

--- cdk/src/config.ts
+++ cdk/src/config.ts
@@ -1,4 +1,8 @@
 export const config = {
+  account: {
+    id: process.env.CDK_DEFAULT_ACCOUNT ?? '',
+    region: process.env.CDK_DEFAULT_REGION ?? ''
+  },
   database: {
     credentials: {
       username: process.env.DATABASE_USERNAME ?? '',
--- cdk/src/stack.ts
+++ cdk/src/stack.ts
@@ -11,8 +11,8 @@ export class CartService extends Stack {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
       env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: process.env.CDK_DEFAULT_REGION
+        account: config.account.id,
+        region: config.account.region
       }
     })
 
