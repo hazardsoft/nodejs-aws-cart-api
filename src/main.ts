c61d2ec2a6433a95fa23bc832323fe25378c63a7
Henadzi Shutko <hazardsoft@gmail.com>
2024-07-25 09:29:40 +0300

    refactor: take server port from env var

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrap } from './bootstrap';
 
+const port = process.env.PORT || 4000;
+
 bootstrap().then((app) => {
-  app.listen(3001);
-  console.log('Server started');
+  app.listen(port);
+  console.log(`Server started at port ${port}`);
 });
